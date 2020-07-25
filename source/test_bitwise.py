8930d7eb3d4e5a6dd2fd5f1854c0849c424c532b
adrianbielsa1 <63594921+adrianbielsa1@users.noreply.github.com>
2020-07-25 15:33:08 -0300

    Added a few tests for "encode" calls

--- source/test_bitwise.py
+++ source/test_bitwise.py
@@ -4,12 +4,31 @@ import unittest
 class TestBitwise(unittest.TestCase):
 
     def setUp(self):
+        self.ordinals_to_encode = {
+            int("00000000", 2) : int("01010101", 2),
+            int("11111111", 2) : int("10101010", 2),
+            int("10101010", 2) : int("00000000", 2),
+            int("01010101", 2) : int("11111111", 2)
+        }
+
         self.ordinals_to_decode = {
             int("01010101", 2) : int("00000000", 2),
             int("10101010", 2) : int("11111111", 2),
             int("00000000", 2) : int("10101010", 2),
             int("11111111", 2) : int("01010101", 2)
         }
+
+    def test_encode(self):
+        for actual, expected in self.ordinals_to_encode.items():
+            # NOTE: "int" objects must be converted to "List" objects before converting
+            # them to "bytearray" objects; otherwise, a "bytearray" is created with the
+            # size of the integer, filled with zeros.
+            actual = bytearray([actual])
+            expected = bytearray([expected])
+
+            actual = bitwise.encode(actual)
+
+            self.assertEqual(actual, expected)
 
     def test_decode(self):
         for actual, expected in self.ordinals_to_decode.items():
